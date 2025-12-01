c43d0cd37557ff3807862754c5ee56ac3c9b52f9
vincent <vincent78@163.com>
2025-12-01 14:48:45 +0800

    feat(): 优化配置

--- config/appConfig.go
+++ config/appConfig.go
@@ -2,6 +2,7 @@ package config
 
 type AppConfig struct {
 	Name  string `yaml:"name" json:"name"`
+	Node  string `yaml:"node" json:"node"`
 	Debug bool   `yaml:"debug" json:"debug"`
 	Proxy string `yaml:"proxy" json:"proxy"`
 }
